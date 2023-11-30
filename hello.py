14e2cc75e91086e1fe9d753288a373671821266a
Burak VARDAR <burakvardar@bvardarWVPF6.vmware.com>
2023-11-30 23:35:01 +0300

    phyton

---
+++ hello.py
@@ -0,0 +1,31 @@
+# Bu kod, "hello world" çıktısı veren bir Python web uygulamasıdır.
+
+# Import gerekli modüller
+from flask import Flask, render_template
+
+# Flask uygulamasını başlat
+app = Flask(__name__)
+
+# Ana rotayı tanımla
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# HTML şablonunu tanımla
+@app.route("/index.html")
+def index_html():
+    return """
+    <html>
+        <head>
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
+
+# Uygulamayı çalıştır
+if __name__ == "__main__":
+    app.run(debug=True)
+
